973a555f847d34d98433fa2ec04b3e8d37ac316a
MouriNaruto <Mouri_Naruto@outlook.com>
2021-03-14 05:08:13 +0800

    Add PWA support.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-module.exports = {
+﻿module.exports = {
   locales: {
     '/en-us/': {
       lang: 'en-US',
@@ -11,7 +11,19 @@ module.exports = {
       description: '为 .NET 设计的最小可启动 Linux 环境'
     }
   },
+  head: [
+    ['link', { rel: 'icon', href: '/assets/icons/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/assets/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/assets/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/assets/icons/mstile-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   themeConfig: {
+    logo: '/assets/image/logo.png',
     editLinks: true,
     search: false,
     smoothScroll: true,
